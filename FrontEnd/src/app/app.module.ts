c8bd5cd9e1f094e8b565c766531bb4d76d6eae19
soumya-ramteke <soumya.r@cgi.com>
2018-10-10 17:34:13 +0530

    added header-component and test-component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { HeaderComponentComponent } from './home-component/header-component/header-component.component';
+import { TestComponentComponent } from './home-component/test-component/test-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    HeaderComponentComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule,
