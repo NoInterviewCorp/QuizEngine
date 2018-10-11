8bb9286f270d08533c742cfe7114c9532321f8ec
akashg524 <akashg524@gmail.com>
2018-10-11 15:27:49 +0530

    signalR

--- BackEnd/asp-back/Hubs/ChatHub.cs
+++ BackEnd/asp-back/Hubs/ChatHub.cs
@@ -1,13 +1,32 @@
-using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
-namespace Asp_Back.Hubs
-{
-    public class ChatHub : Hub
-    {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+namespace Asp_Back.Hubs {
+    public class ChatHub : Hub {
+        public async Task SendMessage (string user, string message) {
+            await Clients.All.SendAsync ("ReceiveMessage", user, message);
         }
+
+        public Task SendMessageToCaller (string message) {
+            return Clients.Caller.SendAsync ("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToGroups (string message) {
+            List<string> groups = new List<string> () { "SignalR Users" };
+            return Clients.Groups (groups).SendAsync ("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync () {
+            await Groups.AddToGroupAsync (Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync ();
+        }
+
+        public override async Task OnDisconnectedAsync (Exception exception) {
+            await Groups.RemoveFromGroupAsync (Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync (exception);
+        }
+        
     }
 }
