85198729e74e0750b17ceaf0441e2b3c407189d9
soumya-ramteke <soumya.r@cgi.com>
2018-10-16 17:45:33 +0530

    routing fixd

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ResultComponent } from './result/result.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponentComponent },
   {path: 'result', component: ResultComponent}
 ];
 
