84be4d20ebb6389399e76cc7ec0991d48b88fc0a
unknown <akashg524@gmail.com>
2018-10-22 14:59:57 +0530

    creating userdata table

---
+++ BackEnd/asp-back/Database/Models/TemporaryData.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learners.Models
+{
+    public class TemporaryData
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string UserName { get; set; }
+        public string QuizId { get; set; }
+        public string TechName { get; set; }
+        public int TpoicCompleted { get; set; }
+        public BloomsLevel Blooms { get; set; }
+        public DateTime AttemptedOn { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
--- BackEnd/asp-back/Database/Persistence/LearnersContext.cs
+++ BackEnd/asp-back/Database/Persistence/LearnersContext.cs
@@ -13,6 +13,8 @@ namespace Learners.Persistence
         public DbSet<Option> Options{get;set;}
         public DbSet<Threshold> Thresholds{get;set;}
         public DbSet<UserData> UserDatas{get;set;}
+        public DbSet<QuizData> QuizDatas{get;set;}
+        public DbSet<TemporaryData> Temporaries{get;set;}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;");
@@ -22,6 +24,7 @@ namespace Learners.Persistence
             modelBuilder.Entity<Technology>().HasMany(tech => tech.Topics).WithOne().HasForeignKey(topic => topic.TechnologyId);
             modelBuilder.Entity<Topic>().HasMany(topic => topic.Questions).WithOne().HasForeignKey(question => question.TopicId);
             modelBuilder.Entity<Question>().HasMany(question => question.Options).WithOne().HasForeignKey(option => option.QuestionId);
+            modelBuilder.Entity<UserData>().HasMany(userdata =>userdata.QuizDatas).WithOne().HasForeignKey(quizdata =>quizdata.UserName);
         }
     }
 }
