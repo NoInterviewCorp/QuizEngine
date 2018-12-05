7a3aba07f7d5916bfc38d642855ad293415e6025
Akash Gupta <akashg524@gmail.com>
2018-12-05 20:05:43 +0530

    added default constructor

--- Evaluation_BackEnd/Source/ContentWrapper/QuestionBatchResponse.cs
+++ Evaluation_BackEnd/Source/ContentWrapper/QuestionBatchResponse.cs
@@ -4,14 +4,13 @@ using Evaluation_BackEnd.Models;
 namespace Evaluation_BackEnd.RabbitMQModels {
     public class QuestionBatchResponse {
         public string username { get; set; }
-        public Dictionary<string, List<Question>> questions;
+        public Dictionary<string, List<Question>> questions = new Dictionary<string, List<Question>>();
         public QuestionBatchResponse()
         {
             
         }
         public QuestionBatchResponse (string _username) {
             username = _username;
-            questions = new Dictionary<string, List<Question>> ();
         }
     }
 }
